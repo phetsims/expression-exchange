42fc87a3906272ef5bee9d355dc621ad903f6d67
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-11-06 09:52:51 -0700

    rename to expression-exchange

---
+++ js/expression-exchange/view/ExpressionExchangeScreenView.js
@@ -0,0 +1,41 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ *
+ * @author Aaron Davis (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+  var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
+
+  /**
+   * @param {ExpressionExchangeModel} expressionExchangeModel
+   * @constructor
+   */
+  function ExpressionExchangeScreenView( expressionExchangeModel ) {
+
+    ScreenView.call( this );
+
+    // Reset All button
+    var resetAllButton = new ResetAllButton( {
+      listener: function() {
+        expressionExchangeModel.reset();
+      },
+      right:  this.layoutBounds.maxX - 10,
+      bottom: this.layoutBounds.maxY - 10
+    } );
+    this.addChild( resetAllButton );
+  }
+
+  return inherit( ScreenView, ExpressionExchangeScreenView, {
+
+    //TODO Called by the animation loop. Optional, so if your view has no animation, please delete this.
+    step: function( dt ) {
+      //TODO Handle view animation here.
+    }
+  } );
+} );
