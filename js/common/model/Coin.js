a15fd85df84f9b8a4a0c96b8901673ae7d378735
jbphet <john.blanco@colorado.edu>
2016-01-20 16:26:05 -0700

    added some of the functionality needed for combining coins

--- js/common/model/Coin.js
+++ js/common/model/Coin.js
@@ -1,7 +1,9 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- * model of a coin which can be represented in the view as a coin image or a mathematical term
+ * This type represents a model of a single or combined coin which can be represented in the view as a coin image or a
+ * mathematical term.  A 'combined' coin is one where other matching coins have been combined with this one, kind of
+ * like a stack of coins, though they are not represented in the view as a stack.
  *
  * @author John Blanco
  */
@@ -13,6 +15,9 @@ define( function( require ) {
   var PropertySet = require( 'AXON/PropertySet' );
   var Vector2 = require( 'DOT/Vector2' );
 
+  // constants
+  var MOVEMENT_SPEED = 100; // in model units (which are basically screen coordinates) per second
+
   /**
    * TODO: document parameters once finalized
    * @constructor
@@ -20,10 +25,29 @@ define( function( require ) {
   function Coin( termInfo ) {
     PropertySet.call( this, {
       position: Vector2.ZERO, // @public
-      userControlled: false // @public, indicate whether user is currently dragging this coin
+      userControlled: false, // @public, indicate whether user is currently dragging this coin
+      coinCount: 1 // @public, number of coins represented
     } );
     this.termInfo = termInfo; // @public, read only
   }
 
-  return inherit( PropertySet, Coin );
+  return inherit( PropertySet, Coin, {
+
+    /**
+     * move to the specified destination, but do so a step at a time rather than all at once
+     * @param {Vector2} destination
+     */
+    travelToDestination: function( destination ){
+      var self = this;
+      var movementTime = self.position.distance( destination ) / MOVEMENT_SPEED * 1000;
+      new TWEEN.Tween( { x: this.position.x, y: this.position.y } )
+        .to( { x: destination.x, y: destination.y }, movementTime )
+        .easing( TWEEN.Easing.Cubic.InOut )
+        .onUpdate( function() {
+          self.position = new Vector2( this.x, this.y );
+        } )
+        .start();
+
+    }
+  } );
 } );
--- js/explore/model/ExpressionExchangeExploreModel.js
+++ js/explore/model/ExpressionExchangeExploreModel.js
@@ -14,6 +14,12 @@ define( function( require ) {
   var PropertySet = require( 'AXON/PropertySet' );
   var ViewMode = require( 'EXPRESSION_EXCHANGE/explore/model/ViewMode' );
 
+  // utility function
+  function coinsOverlap( coin1, coin2 ){
+    var distanceBetweenCenters = coin1.position.distance( coin2.position );
+    return distanceBetweenCenters < ( coin1.termInfo.coinDiameter / 2 ) + ( coin2.termInfo.coinDiameter / 2 ) * 0.65;
+  }
+
   /**
    * @constructor
    */
@@ -39,8 +45,26 @@ define( function( require ) {
       self.totalCents = total;
     }
 
+    // add listeners for updating the total coin value
     this.coins.addItemAddedListener( updateTotal );
     this.coins.addItemRemovedListener( updateTotal );
+
+    // add listeners to handling combining coins
+    this.coins.addItemAddedListener( function( addedCoin ){
+      // TODO: Revisit this and verify that it doesn't leak memory
+      addedCoin.userControlledProperty.onValue( false, function(){
+        var overlappingCoins = self.getOverlappingCoins( addedCoin );
+        if ( overlappingCoins.length > 0 ){
+          for ( var i = 0; i < overlappingCoins.length; i++ ){
+            if ( overlappingCoins[ i ].termInfo === addedCoin.termInfo ){
+              // same type of coin, so combine them
+              addedCoin.travelToDestination( overlappingCoins[ i ].position );
+              break;
+            }
+          }
+        }
+      });
+    } );
   }
 
   return inherit( PropertySet, ExpressionExchangeExploreModel, {
@@ -64,6 +88,18 @@ define( function( require ) {
     reset: function() {
       this.coins.clear();
       PropertySet.prototype.reset.call( this );
+    },
+
+    // @private, gets a list of coins that overlap with the provided coin
+    getOverlappingCoins: function( coin ){
+      assert && assert( this.coins.contains( coin ), 'overlap requested for coin that is not in model' );
+      var overlappingCoins = [];
+      this.coins.forEach( function( potentiallyOverlappingCoin ){
+        if ( coin !== potentiallyOverlappingCoin && coinsOverlap( coin, potentiallyOverlappingCoin ) ){
+          overlappingCoins.push( potentiallyOverlappingCoin );
+        }
+      } );
+      return overlappingCoins;
     }
   } );
 } );
