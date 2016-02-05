3279a5fd740159f30d0d58974c3bf71a2b19a0f9
jbphet <john.blanco@colorado.edu>
2016-02-05 12:32:11 -0700

    backed out addition of TANDEM to list of libs - @samreid is going to change things so that this isn't needed, at least for now

--- js/expression-exchange-config.js
+++ js/expression-exchange-config.js
@@ -36,7 +36,6 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
-    TANDEM: '../../tandem/js',
 
     // this sim
     EXPRESSION_EXCHANGE: '.'
