c7747c0dee3cf97ae23b403410e9e9fd4fe6f302
jbphet <john.blanco@colorado.edu>
2016-02-24 13:54:21 -0700

    added hints to ExpressionNode

--- js/common/view/ExpressionNode.js
+++ js/common/view/ExpressionNode.js
@@ -12,6 +12,7 @@ define( function( require ) {
   // modules
   var EESharedConstants = require( 'EXPRESSION_EXCHANGE/common/EESharedConstants' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Matrix3 = require( 'DOT/Matrix3' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
@@ -24,7 +25,10 @@ define( function( require ) {
   var BACKGROUND_COLOR = EESharedConstants.EXPRESSION_BACKGROUND_COLOR;
   var DEFAULT_SHAPE = Shape.rect( 0, 0, 1, 1 ); // arbitrary initial shape
   var NUM_ZIG_ZAGS = 10;
-  var ZIG_ZAG_X_SIZE = 4; // empirically determined
+  var ZIG_ZAG_X_SIZE = 4; // in screen coordinates, empirically determined
+  var HINT_OFFSET = 3; // in screen coordinates, empirically determined
+  var LEFT_HINT_TRANSLATION = Matrix3.translation( -HINT_OFFSET, 0 );
+  var RIGHT_HINT_TRANSLATION = Matrix3.translation( HINT_OFFSET, 0 );
 
   // TODO: Need to consolidate this with duplicated function in ExpressionHintNode if that node is retained
   // utility function for drawing a vertical zig zag line on a shape between two endpoints
@@ -33,12 +37,12 @@ define( function( require ) {
     var segmentYLength = ( y2 - y1 ) / ( NUM_ZIG_ZAGS - 1 ); // can be negative if line is headed upwards
     var nextPoint = new Vector2( x1, y1 );
     _.times( NUM_ZIG_ZAGS - 1, function( index ) {
-      if ( index === 0 ){
+      if ( index === 0 ) {
         // the first zig is half size so that the line stays centered around a straight line between the two points
         nextPoint.x = nextPoint.x + ZIG_ZAG_X_SIZE / 2 * ( zigRightFirst ? 1 : -1 );
         nextPoint.y = nextPoint.y + segmentYLength / 2;
       }
-      else{
+      else {
         nextPoint.x = nextPoint.x + ( nextPoint.x > x1 ? -1 : 1 ) * ZIG_ZAG_X_SIZE;
         nextPoint.y = nextPoint.y + segmentYLength;
       }
@@ -60,6 +64,14 @@ define( function( require ) {
     var backgroundShape = DEFAULT_SHAPE;
     var backgroundPath = new Path( backgroundShape, { fill: BACKGROUND_COLOR } );
     this.addChild( backgroundPath );
+
+    // left and right 'hints' that are used to indicate to the user that a coin term can be added
+    var leftHintShape = DEFAULT_SHAPE;
+    var leftHintNode = new Path( leftHintShape, { fill: BACKGROUND_COLOR } );
+    this.addChild( leftHintNode );
+    var rightHintShape = DEFAULT_SHAPE;
+    var rightHintNode = new Path( leftHintShape, { fill: BACKGROUND_COLOR } );
+    this.addChild( rightHintNode );
 
     // layer where the plus symbols go
     var plusSymbolsLayer = new Node();
@@ -123,7 +135,7 @@ define( function( require ) {
       }
     }
 
-    // update the shape if the height, width, or hint states change
+    // update the shape of the background if the height, width, or hint states of the expression change
     Property.multilink(
       [ expression.widthProperty, expression.heightProperty, expression.leftHintActiveProperty, expression.rightHintActiveProperty ],
       updateShapeAndPlusSymbols
@@ -131,13 +143,43 @@ define( function( require ) {
 
     // update the position when the expression moves
     expression.upperLeftCornerProperty.link( function( upperLeftCorner ) {
-      self.left = upperLeftCorner.x;
-      self.top = upperLeftCorner.y;
+      self.x = upperLeftCorner.x;
+      self.y = upperLeftCorner.y;
     } );
 
     // update whenever coin terms are added or removed
     expression.coinTerms.addItemAddedListener( updateShapeAndPlusSymbols );
     expression.coinTerms.addItemRemovedListener( updateShapeAndPlusSymbols );
+
+    // update the visibility of the left and right hints
+    expression.leftHintActiveProperty.linkAttribute( leftHintNode, 'visible' );
+    expression.rightHintActiveProperty.linkAttribute( rightHintNode, 'visible' );
+
+    // update the shape of the left and right hints
+    Property.multilink( [ expression.heightProperty, expression.widthProperty, expression.leftHintWidthProperty ],
+      function( expressionHeight, expressionWidth, hintWidth ) {
+        leftHintShape = new Shape();
+        leftHintShape.moveTo( -hintWidth, 0 );
+        leftHintShape.lineTo( 0, 0 );
+        addVerticalZigZagLine( leftHintShape, 0, 0, 0, expressionHeight, true );
+        leftHintShape.lineTo( -hintWidth, expressionHeight );
+        leftHintShape.close();
+        leftHintShape = leftHintShape.transformed( LEFT_HINT_TRANSLATION );
+        leftHintNode.shape = leftHintShape;
+      }
+    );
+    Property.multilink( [ expression.heightProperty, expression.widthProperty, expression.rightHintWidthProperty ],
+      function( expressionHeight, expressionWidth, hintWidth ) {
+        rightHintShape = new Shape();
+        rightHintShape.moveTo( expressionWidth, 0 );
+        addVerticalZigZagLine( rightHintShape, expressionWidth, 0, expressionWidth, expressionHeight, true );
+        rightHintShape.lineTo( expressionWidth + hintWidth, expressionHeight );
+        rightHintShape.lineTo( expressionWidth + hintWidth, 0 );
+        rightHintShape.close();
+        rightHintShape = rightHintShape.transformed( RIGHT_HINT_TRANSLATION );
+        rightHintNode.shape = rightHintShape;
+      }
+    );
 
     // do the initial update
     updateShapeAndPlusSymbols();
