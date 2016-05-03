191614049c8a419e87f0746bc5293fa4786674e7
jbphet <john.blanco@colorado.edu>
2016-05-03 12:59:17 -0600

    lint fix

--- js/expression-exchange-main.js
+++ js/expression-exchange-main.js
@@ -13,7 +13,6 @@ define( function( require ) {
   var EEExploreScreen = require( 'EXPRESSION_EXCHANGE/explore/EEExploreScreen' );
   var EEGameScreen = require( 'EXPRESSION_EXCHANGE/game/EEGameScreen' );
   var EEVariablesScreen = require( 'EXPRESSION_EXCHANGE/variables/EEVariablesScreen' );
-  var expressionExchange = require( 'EXPRESSION_EXCHANGE/expressionExchange' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
 
