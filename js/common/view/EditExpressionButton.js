790765555817079af9256d908605e6034ed7e2d6
jbphet <john.blanco@colorado.edu>
2016-06-15 15:36:53 -0600

    replaced edit icon with Font Awesome version

--- js/common/view/EditExpressionButton.js
+++ js/common/view/EditExpressionButton.js
@@ -8,12 +8,9 @@ define( function( require ) {
 
   // modules
   var expressionExchange = require( 'EXPRESSION_EXCHANGE/expressionExchange' );
-  var Image = require( 'SCENERY/nodes/Image' );
+  var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var inherit = require( 'PHET_CORE/inherit' );
   var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
-
-  // images
-  var editExpressionIcon = require( 'image!EXPRESSION_EXCHANGE/edit-icon.png' );
 
   /**
    * @constructor
@@ -23,7 +20,7 @@ define( function( require ) {
     options = options || {};
 
     // the following options can't be overridden
-    options.content = new Image( editExpressionIcon, { scale: 0.3 } ); // scale empirically determined
+    options.content = new FontAwesomeNode( 'pencil_square_o', { scale: 0.3 } ); // scale empirically determined
     options.xMargin = 3; // empirically determined
     options.yMargin = 4; // empirically determined
     options.baseColor = 'white';
