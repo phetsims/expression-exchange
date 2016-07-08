eb0bc1d42c1f6a5aa3a6d7bcec15ba63a8380867
jbphet <john.blanco@colorado.edu>
2016-07-08 12:14:43 -0600

    fixed incorrect font

--- js/variables/view/EEVariablesIconNode.js
+++ js/variables/view/EEVariablesIconNode.js
@@ -13,13 +13,17 @@ define( function( require ) {
   var expressionExchange = require( 'EXPRESSION_EXCHANGE/expressionExchange' );
   var inherit = require( 'PHET_CORE/inherit' );
   var MathSymbolFont = require( 'SCENERY_PHET/MathSymbolFont' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Screen = require( 'JOIST/Screen' );
   var SubSupText = require( 'SCENERY_PHET/SubSupText' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   // constants
   var ICON_SIZE = Screen.HOME_SCREEN_ICON_SIZE;
   var BACKGROUND_COLOR = EESharedConstants.NON_GAME_SCREENS_BACKGROUND_COLOR;
+  var FONT_SIZE = 100;
 
   /**
    * @constructor
@@ -29,13 +33,17 @@ define( function( require ) {
     // create the background
     Rectangle.call( this, 0, 0, ICON_SIZE.width, ICON_SIZE.height, { fill: BACKGROUND_COLOR } );
 
-    // add the equation node
-    this.addChild( new SubSupText( '3x<sup>2</sup> - x<sup>2</sup>', {
-      font: new MathSymbolFont( 100 ),
-      centerX: ICON_SIZE.width / 2,
-      centerY: ICON_SIZE.height * 0.45,
-      supScale: 0.5
+    // create and add the equation node
+    var equationNode = new Node();
+    equationNode.addChild( new Text( '3', { font: new PhetFont( FONT_SIZE ) } ) );
+    equationNode.addChild( new SubSupText( 'x<sup>2</sup> - x<sup>2</sup>', {
+      font: new MathSymbolFont( FONT_SIZE ),
+      supScale: 0.5,
+      left: equationNode.width
     } ) );
+    equationNode.centerX = ICON_SIZE.width / 2;
+    equationNode.centerY = ICON_SIZE.height * 0.45;
+    this.addChild( equationNode );
   }
 
   expressionExchange.register( 'EEVariablesIconNode', EEVariablesIconNode );
