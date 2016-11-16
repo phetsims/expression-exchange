23797b40be9cad25e698637b736ef52b3a8bfafb
jbphet <john.blanco@colorado.edu>
2016-11-16 16:05:41 -0700

    added some nodes that look like collection areas

--- js/game/view/EEGameLevelView.js
+++ js/game/view/EEGameLevelView.js
@@ -10,12 +10,21 @@ define( function( require ) {
   var ExpressionManipulationView = require( 'EXPRESSION_EXCHANGE/common/view/ExpressionManipulationView' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
+  /**
+   * @param {EEGameLevelModel} levelModel
+   * @param {Bounds2} screenLayoutBounds
+   * @param {Property.<Bounds2>} visibleBoundsProperty
+   * @constructor
+   */
   function EEGameLevelView( levelModel, screenLayoutBounds, visibleBoundsProperty ) {
     Node.call( this );
 
+    // TODO: Temporarily using this descriptor until we have something to generate them.
     var coinTermCreatorDescriptors = CoinTermCreatorBox.BASIC_SCREEN_CONFIG;
 
+    // add the coin term creator box
     var coinTermCreatorBox = new CoinTermCreatorBox(
       coinTermCreatorDescriptors,
       levelModel,
@@ -27,7 +36,33 @@ define( function( require ) {
     );
     this.addChild( coinTermCreatorBox );
 
-    this.addChild( new ExpressionManipulationView( levelModel, Bounds2.EMPTY, visibleBoundsProperty ) );
+    // TODO: Temp for demo purposes - add some boxes that look like the collection area
+    var collectionAreaWidth = 200;
+    var collectionAreaHeight = 100;
+    var topCollectionArea = new Rectangle( 0, 0, collectionAreaWidth, collectionAreaHeight, {
+      fill: 'white',
+      stroke: 'gray',
+      top: 20,
+      right: screenLayoutBounds.maxX - 20
+    } );
+    this.addChild( topCollectionArea );
+    var middleCollectionArea = new Rectangle( 0, 0, collectionAreaWidth, collectionAreaHeight, {
+      fill: 'white',
+      stroke: 'gray',
+      top: topCollectionArea.bottom + 20,
+      right: topCollectionArea.right
+    } );
+    this.addChild( middleCollectionArea );
+    var bottomCollectionArea = new Rectangle( 0, 0, collectionAreaWidth, collectionAreaHeight, {
+      fill: 'white',
+      stroke: 'gray',
+      top: middleCollectionArea.bottom + 20,
+      right: middleCollectionArea.right
+    } );
+    this.addChild( bottomCollectionArea );
+
+    // add the view area where the user will interact with coin terms and expressions
+    this.addChild( new ExpressionManipulationView( levelModel, coinTermCreatorBox.bounds, visibleBoundsProperty ) );
   }
 
   expressionExchange.register( 'EEGameLevelView', EEGameLevelView );
