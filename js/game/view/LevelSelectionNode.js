ac05ab180f79305ff6377414af3d08073fb84a0f
Andrea Lin <andrealin2018@gmail.com>
2018-03-30 13:35:36 -0600

    Change use of LevelSelectionItemNode to deprecated version, see https://github.com/phetsims/vegas/issues/59

--- js/game/view/LevelSelectionNode.js
+++ js/game/view/LevelSelectionNode.js
@@ -13,7 +13,7 @@ define( function( require ) {
   var EESharedConstants = require( 'EXPRESSION_EXCHANGE/common/EESharedConstants' );
   var expressionExchange = require( 'EXPRESSION_EXCHANGE/expressionExchange' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var LevelSelectionItemNode = require( 'VEGAS/LevelSelectionItemNode' );
+  var LevelSelectionItemNodeDeprecated = require( 'VEGAS/LevelSelectionItemNodeDeprecated' );
   var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
@@ -76,7 +76,7 @@ define( function( require ) {
 
     var buttons = new Array( options.numLevels );
     for ( var i = 0; i < options.numLevels; i++ ) {
-      buttons[ i ] = new LevelSelectionItemNode(
+      buttons[ i ] = new LevelSelectionItemNodeDeprecated(
         iconNodes[ i ],
         options.numStarsOnButtons,
         createLevelStartFunction( i ),
