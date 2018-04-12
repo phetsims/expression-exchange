3c217d40e91845ff5a98589128f17d6636b0c686
Chris Malley <cmalley@pixelzoom.com>
2018-04-12 10:35:10 -0600

    revert LevelSelectionItemNodeDeprecated to LevelSelectionItemNode
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/game/view/LevelSelectionNode.js
+++ js/game/view/LevelSelectionNode.js
@@ -13,7 +13,7 @@ define( function( require ) {
   var EESharedConstants = require( 'EXPRESSION_EXCHANGE/common/EESharedConstants' );
   var expressionExchange = require( 'EXPRESSION_EXCHANGE/expressionExchange' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var LevelSelectionItemNodeDeprecated = require( 'VEGAS/LevelSelectionItemNodeDeprecated' );
+  var LevelSelectionItemNode = require( 'VEGAS/LevelSelectionItemNode' );
   var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
@@ -76,7 +76,7 @@ define( function( require ) {
 
     var buttons = new Array( options.numLevels );
     for ( var i = 0; i < options.numLevels; i++ ) {
-      buttons[ i ] = new LevelSelectionItemNodeDeprecated(
+      buttons[ i ] = new LevelSelectionItemNode(
         iconNodes[ i ],
         options.numStarsOnButtons,
         createLevelStartFunction( i ),
