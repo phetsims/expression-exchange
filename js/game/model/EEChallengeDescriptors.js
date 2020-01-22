a5d51cd3e173762f41a8128e6b30bcb5233c620d
phet-dev <phetadmin@gmail.com>
2020-01-22 03:24:00 -0700

    update copyright dates from daily grunt work

--- js/game/model/EEChallengeDescriptors.js
+++ js/game/model/EEChallengeDescriptors.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * an array of static objects that describe the Expression Exchange challenges, organized by game level
--- js/game/view/EEGameLevelIconFactory.js
+++ js/game/view/EEGameLevelIconFactory.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * static object that provides functions for creating nodes that represent the coins used in the simulation
