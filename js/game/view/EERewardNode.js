73d16c887b6a481d3ae9e2e4dd30d60d5cab929b
pixelzoom <cmalley@pixelzoom.com>
2020-09-17 00:16:53 -0600

    convert RewardDialog to ES6, https://github.com/phetsims/vegas/issues/82

--- js/game/view/EERewardNode.js
+++ js/game/view/EERewardNode.js
@@ -1,4 +1,5 @@
 // Copyright 2017-2020, University of Colorado Boulder
+
 /**
  * The reward that is displayed when all levels have been correctly completed.  For testing, the simulation can be run
  * with the 'reward' query parameter to show the reward each time a level is successfully completed.
@@ -6,7 +7,6 @@
  * @author John Blanco
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import FaceNode from '../../../../scenery-phet/js/FaceNode.js';
 import MathSymbolFont from '../../../../scenery-phet/js/MathSymbolFont.js';
 import StarNode from '../../../../scenery-phet/js/StarNode.js';
@@ -24,30 +24,29 @@ const STAR_OUTER_RADIUS = 20;
 const STAR_INNER_RADIUS = STAR_OUTER_RADIUS / 2;
 const VARIABLE_FONT = new MathSymbolFont( 36 );
 
-/**
- * @constructor
- */
-function EERewardNode() {
+class EERewardNode extends RewardNode {
 
-  const nodes = [];
+  constructor() {
 
-  // add nodes that look like smiley faces, stars, and variables
-  nodes.push( new FaceNode( FACE_DIAMETER ) );
-  nodes.push( new StarNode( { outerRadius: STAR_OUTER_RADIUS, innerRadius: STAR_INNER_RADIUS } ) );
-  nodes.push( new Text( 'x', { font: VARIABLE_FONT } ) );
-  nodes.push( new Text( 'y', { font: VARIABLE_FONT } ) );
-  nodes.push( new Text( 'z', { font: VARIABLE_FONT } ) );
+    // add nodes that look like smiley faces, stars, and variables
+    const nodes = [
+      new FaceNode( FACE_DIAMETER ),
+      new StarNode( { outerRadius: STAR_OUTER_RADIUS, innerRadius: STAR_INNER_RADIUS } ),
+      new Text( 'x', { font: VARIABLE_FONT } ),
+      new Text( 'y', { font: VARIABLE_FONT } ),
+      new Text( 'z', { font: VARIABLE_FONT } )
+    ];
 
-  // add coin images
-  CoinTermTypeID.VALUES.forEach( function( coinTermTypeID ) {
-    if ( coinTermTypeID !== CoinTermTypeID.CONSTANT ) {
-      nodes.push( CoinNodeFactory.createImageNode( coinTermTypeID, COIN_RADIUS, true ) );
-    }
-  } );
-  RewardNode.call( this, { nodes: RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES ) } );
+    // add a node for each coin type
+    CoinTermTypeID.VALUES.forEach( coinTermTypeID => {
+      if ( coinTermTypeID !== CoinTermTypeID.CONSTANT ) {
+        nodes.push( CoinNodeFactory.createImageNode( coinTermTypeID, COIN_RADIUS, true ) );
+      }
+    } );
+
+    super( { nodes: RewardNode.createRandomNodes( nodes, NUMBER_OF_NODES ) } );
+  }
 }
 
 expressionExchange.register( 'EERewardNode', EERewardNode );
-
-inherit( RewardNode, EERewardNode );
 export default EERewardNode;
