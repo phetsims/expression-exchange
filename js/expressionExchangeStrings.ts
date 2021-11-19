d139606517b2b89d36c1bd41221d234dbbfb20a3
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:40 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/expressionExchangeStrings.ts
@@ -0,0 +1,35 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import expressionExchange from './expressionExchange.js';
+
+type StringsType = {
+  'coinValues': string,
+  'done': string,
+  'numberCentsPattern': string,
+  'expression-exchange': {
+    'title': string
+  },
+  'explore': string,
+  'basics': string,
+  'negatives': string,
+  'game': string,
+  'myCollection': string,
+  'allCoefficients': string,
+  'total': string,
+  'variableValues': string,
+  'variables': string,
+  'levelNumberPattern': string,
+  'next': string,
+  'youCompletedAllLevels': string
+};
+
+const expressionExchangeStrings = getStringModule( 'EXPRESSION_EXCHANGE' ) as StringsType;
+
+expressionExchange.register( 'expressionExchangeStrings', expressionExchangeStrings );
+
+export default expressionExchangeStrings;
