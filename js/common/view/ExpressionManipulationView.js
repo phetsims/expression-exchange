1ddac33731be801fe4c0916b60673616ac70e8ab
phet-dev <phetadmin@gmail.com>
2022-03-05 03:22:42 -0700

    update copyright dates from daily grunt work

--- js/common/view/ExpressionManipulationView.js
+++ js/common/view/ExpressionManipulationView.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * a view node that allows the user to interact with coin terms to create and manipulate expressions
