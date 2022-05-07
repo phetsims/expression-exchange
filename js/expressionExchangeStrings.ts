529fe7748adcddf1e5aecc00ab21cd98f7b7eb3b
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:22 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/expressionExchangeStrings.ts
+++ js/expressionExchangeStrings.ts
@@ -8,24 +8,24 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import expressionExchange from './expressionExchange.js';
 
 type StringsType = {
-  'coinValues': string,
-  'done': string,
-  'numberCentsPattern': string,
+  'coinValues': string;
+  'done': string;
+  'numberCentsPattern': string;
   'expression-exchange': {
-    'title': string
-  },
-  'explore': string,
-  'basics': string,
-  'negatives': string,
-  'game': string,
-  'myCollection': string,
-  'allCoefficients': string,
-  'total': string,
-  'variableValues': string,
-  'variables': string,
-  'levelNumberPattern': string,
-  'next': string,
-  'youCompletedAllLevels': string
+    'title': string;
+  };
+  'explore': string;
+  'basics': string;
+  'negatives': string;
+  'game': string;
+  'myCollection': string;
+  'allCoefficients': string;
+  'total': string;
+  'variableValues': string;
+  'variables': string;
+  'levelNumberPattern': string;
+  'next': string;
+  'youCompletedAllLevels': string;
 };
 
 const expressionExchangeStrings = getStringModule( 'EXPRESSION_EXCHANGE' ) as StringsType;
