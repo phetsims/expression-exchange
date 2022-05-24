6614765d917079ebd443b753e1b45038f871b097
phet-dev <phetadmin@gmail.com>
2022-05-24 03:34:43 -0600

    update copyright dates from daily grunt work

--- js/common/view/CoinTermIconNode.js
+++ js/common/view/CoinTermIconNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * a Scenery node that looks like a small version of the coin terms, used in the collection display
