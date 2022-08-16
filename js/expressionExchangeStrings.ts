98f34ca7e4884717f2df33b583185498ba2ee5b4
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/expressionExchangeStrings.ts
+++ js/expressionExchangeStrings.ts
@@ -5,27 +5,44 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import expressionExchange from './expressionExchange.js';
 
 type StringsType = {
   'coinValues': string;
+  'coinValuesProperty': TReadOnlyProperty<string>;
   'done': string;
+  'doneProperty': TReadOnlyProperty<string>;
   'numberCentsPattern': string;
+  'numberCentsPatternProperty': TReadOnlyProperty<string>;
   'expression-exchange': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'explore': string;
+  'exploreProperty': TReadOnlyProperty<string>;
   'basics': string;
+  'basicsProperty': TReadOnlyProperty<string>;
   'negatives': string;
+  'negativesProperty': TReadOnlyProperty<string>;
   'game': string;
+  'gameProperty': TReadOnlyProperty<string>;
   'myCollection': string;
+  'myCollectionProperty': TReadOnlyProperty<string>;
   'allCoefficients': string;
+  'allCoefficientsProperty': TReadOnlyProperty<string>;
   'total': string;
+  'totalProperty': TReadOnlyProperty<string>;
   'variableValues': string;
+  'variableValuesProperty': TReadOnlyProperty<string>;
   'variables': string;
+  'variablesProperty': TReadOnlyProperty<string>;
   'levelNumberPattern': string;
+  'levelNumberPatternProperty': TReadOnlyProperty<string>;
   'next': string;
+  'nextProperty': TReadOnlyProperty<string>;
   'youCompletedAllLevels': string;
+  'youCompletedAllLevelsProperty': TReadOnlyProperty<string>;
 };
 
 const expressionExchangeStrings = getStringModule( 'EXPRESSION_EXCHANGE' ) as StringsType;
