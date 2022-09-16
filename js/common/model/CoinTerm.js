1d5b406e2565051e98b13f57f2ef1a8a0a991e65
phet-dev <phetadmin@gmail.com>
2022-09-16 04:44:04 -0600

    update copyright dates from daily grunt work

--- js/common/model/CoinTerm.js
+++ js/common/model/CoinTerm.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * This type represents a model of a single or combined coin which can be represented in the view as a coin image or a
